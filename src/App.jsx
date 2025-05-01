1d12d0c15a760b3937f957889646b555b46ab2d3
SAHIL VERMA <132760049+sahilverma1512@users.noreply.github.com>
2025-05-01 12:38:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
         </p>
       </div>
       <Canvas camera={{ fov: 12, position: [0, -10, 220] }}>
-        {/* <OrbitControls/> */}
+        <OrbitControls/>
         <Environment
           files={[
             "https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr",
